4fd4d93208a785e71c213c11b458ca871f80c8f1
Shiyam.K.S <k.s.shiyam2002@gmail.com>
2023-06-30 19:49:36 +0530

    form

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import './Background.css'
 function App(){
     return(
         <>
-           <div>
+           <div className='Bio-Data'>
             <form>
+                <br/>
+                <div className='inputs'>
+                    <input type='text' placeholder='Name' />    <br/>
+                    <input type='number' placeholder='Age'/>    <br/>
+                    <input type='mail' placeholder='Email'/>    <br/>
+                    <input type='number' placeholder='PhoneNo'/><br/>
+                </div>
+                    <input type='button' placeholder='Submit' className='button'/>
                 
             </form>
            </div>
