3a48907b2af800505f49357964925ac4ccdb98a7
Shiyam.K.S <k.s.shiyam2002@gmail.com>
2023-07-01 18:47:32 +0530

    form design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import './Background.css'
+import './Form.css'
 
 function App(){
     return(
         <>
-           <div className='Bio-Data'>
+           <div className='Bio-Data' 
+           style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh',
+          }}    
+           >
             <form>
                 <br/>
-                <div className='inputs'>
+                <div className='inputs' 
+                >
                     <input type='text' placeholder='Name' />    <br/>
                     <input type='number' placeholder='Age'/>    <br/>
                     <input type='mail' placeholder='Email'/>    <br/>
                     <input type='number' placeholder='PhoneNo'/><br/>
                 </div>
-                    <input type='button' placeholder='Submit' className='button'/>
+                    <input className='button' type='button' placeholder='Submit'/>
                 
             </form>
            </div>
