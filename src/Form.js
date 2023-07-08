02b3c1a3a5a02dd6bc1100836895b7bd352564f1
Shiyam.K.S <k.s.shiyam2002@gmail.com>
2023-07-08 19:45:02 +0530

    new update

--- src/Form.js
+++ src/Form.js
@@ -1,16 +1,19 @@
 import './Form.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 export let Detail=()=>{
     return(
         <>
         <div id='Bio-Data'>
             <form >
-                <br/>
+                <h3 className='title'>Detail</h3>
                 <div id='inputs' >
-                    <input type='text' placeholder='Name' />    <br></br>
-                    <input type='number' placeholder='Age'/>    <br></br>
-                    <input type='mail' placeholder='Email'/>    <br/>
-                    <input type='number' placeholder='PhoneNo'/><br/>
+                
+                <input type='text' placeholder='Name' className="form-control"/ ><i class="bi bi-person"/>   <br></br>
+                    <input type='number' placeholder='Age' className="form-control"/>    <br></br>
+                    <input type='mail' placeholder='Email' className="form-control"/>    <br/>
+                    <input type='number' placeholder='PhoneNo' className="form-control"/><br/>
                 </div>
                     <button className='button' >Submit</button>
                 
